2caef5b75cfe76c5156ecbe3eb11798a20d6b2c5
10KJVN <90614291+10KJVN@users.noreply.github.com>
2025-07-19 22:29:31 +0200

    missed a function in Interpolation include.
    Can start cleaning up vertex and fragment stage after this.

--- shaders/includes/interpolation.glsl
+++ shaders/includes/interpolation.glsl
@@ -5,3 +5,8 @@
 vec2 quinticInterpolation(vec2 t) {
   return t * t * t * (t * (t * vec2(6) - vec2(15)) + vec2(10));
 }
+
+// Derivative of above function
+vec2 quinticDerivative(vec2 t) {
+  return vec2(30) * t * t * (t * (t - vec2(2)) + vec2(1));
+}
